6efb5900a2c9c7c81c852438c1d9007338346fa8
Kalesh Kaladharan <kalesh.13@gmail.com>
2021-08-27 19:14:47 +0530

    _model should be static and singleton

--- src/mongoose/mongoModel.ts
+++ src/mongoose/mongoModel.ts
@@ -2,7 +2,7 @@ import { BaseModel as Model } from '../baseModel';
 import { IMongoModel as IModel } from '@rheas/contracts/database';
 import { Mongoose as Con, Model as Mod, Schema, Document as Doc } from 'mongoose';
 
-abstract class MongoModel<T extends Doc> extends Model<Con, Mod<T>, Schema> implements IModel<T> {
+abstract class MongoModel<T extends Doc> extends Model<Con, Mod<T>, Schema<T, Mod<T>>> implements IModel<T> {
     /**
      * Creates a new model from the schema with the class name.
      *
